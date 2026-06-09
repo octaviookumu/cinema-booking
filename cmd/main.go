92d3e9c65a980a361b8fbc034e473662f5db88cc
octaviookumu <octaviookumu@gmail.com>
2026-06-10 00:27:15 +0300

    feat: create web api to use redis store

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /movies", listMovies)
+
+	mux.Handle("GET /", http.FileServer(http.Dir("static")))
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var movies = []movieResponse{
+	{ID: "inception", Title: "Inception", Rows: 5, SeatsPerRow: 8},
+	{ID: "dune", Title: "Dune", Rows: 4, SeatsPerRow: 6},
+}
+
+// handler for listing movies
+func listMovies(w http.ResponseWriter, r *http.Request){
+	WriteJSON(w, http.StatusOK, movies)
+}
+
+// helper function to write JSON responses
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(v)
+}
+
+// what the frontend is expecting
+type movieResponse struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Rows int `json:"rows"`
+	SeatsPerRow int `json:"seats_per_row"`
 }
